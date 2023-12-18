f7469dfbf3436e3008676871560bfa435e600504
Akash-Gottimukkala <76110994+Akash-Gottimukkala@users.noreply.github.com>
2023-12-18 22:15:16 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function App() {
+  const [currentUser, setCurrentUser] = useState();
+  const [paymentStatus, setPaymentStatus] = useState(false);
+  const navigate = useNavigate();
+
+  const inputProps = {
+    padding: "8px",
+    borderRadius: "10px",
+    margin: "0.2rem",
+  };
+  const [selectedBatch, setSelectedBatch] = useState("");
+
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      if (!localStorage.getItem("user-data")) {
+        navigate("/login");
+      }
+      setCurrentUser(await JSON.parse(localStorage.getItem("user-data")));
+    };
+    getCurrentUser();
+  }, [navigate]);
+
+  useEffect(() => {
+    const getPaymentStatus = async () => {
+      const user = await axios.get(
+        `http://localhost:5000/api/auth/status/:${currentUser._id}`
+      );
+
+      setPaymentStatus(user.data.paymentStatus);
+    };
+    getPaymentStatus();
+  }, [currentUser]);
+
+  const handlePaymentUpdate = async (event) => {
+    event.preventDefault();
+    if (currentUser.paymentStatus === true || paymentStatus === true) {
+      return;
+    }
+    const user = await axios.patch("http://localhost:5000/api/auth/update", {
+      email: currentUser.email,
+      paymentStatus: true,
+    });
+    console.log(user.data);
+    setPaymentStatus(true);
+  };
+
+  const handleLogOut = async () => {
+    localStorage.removeItem("user-data");
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <div className="login2">
+        <select
+          value={selectedBatch}
+          onChange={(e) => setSelectedBatch(e.target.value)}
+          style={inputProps}
+        >
+          <option value="">Select Batch</option>
+          <option value="6-7AM">6-7AM</option>
+          <option value="7-8AM">7-8AM</option>
+          <option value="8-9AM">8-9AM</option>
+          <option value="5-6PM">5-6PM</option>
+        </select>
+        <button>Update Batch</button>
+        <div>
+          <div>
+            Payment Status: {paymentStatus === false ? "Not Paid" : "Paid"}{" "}
+          </div>
+          {paymentStatus === false ? (
+            <button onClick={handlePaymentUpdate}> Pay now</button>
+          ) : (
+            <span>Fee Paid</span>
+          )}
+        </div>
+        <div>
+          <button onClick={handleLogOut}>LogOut</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
